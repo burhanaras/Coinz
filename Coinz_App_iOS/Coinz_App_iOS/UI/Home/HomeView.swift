c3fb42c88be81158a3b65f313649bd0e1e22975a
Burhan Aras <burhanaras@live.com>
2021-12-26 20:14:48 +0300

    Coins are listed on home with pull to refresh functionality.

--- Coinz_App_iOS/Coinz_App_iOS/UI/Home/HomeView.swift
+++ Coinz_App_iOS/Coinz_App_iOS/UI/Home/HomeView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var viewModel = HomeViewModel()
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List(viewModel.coins){ coin in
+                Text(coin.name)
+            }
             .task {
                 viewModel.loadData()
             }
+            .refreshable {
+                viewModel.loadData()
+            }
+            .navigationTitle("₿ Coinz App")
+        }
     }
 }
 
