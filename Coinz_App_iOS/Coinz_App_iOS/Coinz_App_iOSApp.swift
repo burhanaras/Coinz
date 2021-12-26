c5fba307e826743fc71b321aff8cffc133c65abf
Burhan Aras <burhanaras@live.com>
2021-12-26 19:39:10 +0300

    Initial commit: Bismillah

---
+++ Coinz_App_iOS/Coinz_App_iOS/Coinz_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coinz_App_iOSApp.swift
+//  Coinz_App_iOS
+//
+//  Created by Burhan Aras on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Coinz_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
