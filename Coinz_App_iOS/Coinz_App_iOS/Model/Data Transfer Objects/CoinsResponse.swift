025bc6724501322a90f84866baeb752b9a7c7008
Burhan Aras <burhanaras@live.com>
2021-12-26 19:56:22 +0300

    DTO's are added

---
+++ Coinz_App_iOS/Coinz_App_iOS/Model/Data Transfer Objects/CoinsResponse.swift
@@ -0,0 +1,27 @@
+//
+//  CoinsResponse.swift
+//  Coinz_App_iOS
+//
+//  Created by Burhan Aras on 26.12.2021.
+//
+
+import Foundation
+
+struct CoinDTO: Codable {
+    let uuid: String
+    let symbol: String
+    let name: String
+    let iconUrl: String
+    let price: String
+    let marketCap: String
+    let change: String
+}
+
+struct CoinsDataDTO: Codable {
+    let coins: [CoinDTO]
+}
+
+struct CoinsResponse: Codable {
+    let status: String
+    let data: CoinsDataDTO
+}
