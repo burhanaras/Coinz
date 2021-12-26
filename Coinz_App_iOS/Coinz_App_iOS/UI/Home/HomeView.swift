750189cf18d3a88846179d67d0f68300a8008ac2
Burhan Aras <burhanaras@live.com>
2021-12-26 19:54:44 +0300

    Home view is added.

---
+++ Coinz_App_iOS/Coinz_App_iOS/UI/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Coinz_App_iOS
+//
+//  Created by Burhan Aras on 26.12.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Coinz_App_iOS/Coinz_App_iOS/UI/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  Coinz_App_iOS
+//
+//  Created by Burhan Aras on 26.12.2021.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    
+}
